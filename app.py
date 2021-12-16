0011e03fece2ccd9c5cf27accbf4630fe5495bcf
Santiago Yepes <ysantiago@unicauca.edu.co>
2021-12-16 01:54:38 -0500

    IMPL: Deleting Trip from firebase and redirect to trip details

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from threading import Semaphore
 import firebase_admin
+import json
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, Response, make_response
 from firebase_admin import credentials
 from firebase_admin import db
 
@@ -18,12 +19,44 @@ firebase_admin.initialize_app(cred, {
 def query_trips():
 	ref = db.reference('/tripList')
 	tripList = ref.get()
-
-	smartphoneTrips = [trip for trip in tripList.values() if trip['device'] == 'Smartphone']
-	raspberryTrips = [trip for trip in tripList.values() if trip['device'] == 'Raspberry']
 	data = {
 		'title': 'Información de trayectos',
-		'smartphoneTrips': smartphoneTrips,
-		'raspberryTrips': raspberryTrips		
+		'trips': {} if (tripList == None) else tripList
+		#'smartphoneTrips': smartphoneTrips,
+		#'raspberryTrips': raspberryTrips		
 	}
 	return render_template('index.html', **data)
+
+@app.route("/trips/details/<id>", methods=["GET"])
+def trip_details(id):
+	return "<p>"+str(id)+"</p>"
+
+
+@app.route("/removeTrip", methods=["DELETE"])
+def remove_trip():
+	request_data = request.get_json()
+	idTrip = request_data['idTrip']
+	device = request_data['device']
+	response_data = {
+			'result': ''
+		}	
+	try:
+		#raise Exception('spam', 'eggs')
+		ref = db.reference('/tripList/'+str(idTrip))
+		ref.delete()
+		ref = db.reference('/tripData/{}/{}'.format(device, idTrip))
+		ref.delete()
+
+		response_data['result'] = 'success'
+		res = make_response(json.dumps(response_data), 200)
+		res.headers['Content-Type'] = 'application/json'
+		print("Data deleted: {} {}".format(request_data['device'], request_data['idTrip'] ) )
+		return res
+
+	except:
+		response_data['result'] = 'error'
+		res = make_response(json.dumps(response_data), 200)
+		res.headers['Content-Type'] = 'application/json'
+		return res
+
+	
--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,25 @@
 const tabs = document.getElementById('tabs')
 const tables = document.querySelector('.table-container').children;
+const removeBtns = document.querySelectorAll('.icon.icon-delete');
+const detailsBtns = document.querySelectorAll('.icon.icon-details');
+const modal = document.getElementById('modal-confirmation');
+let tripToRemove = "";
+let row = -1;
+let device = "smartphone";
 
-const handleTabsClick = (e) => {
+removeBtns.forEach(btn => {
+  btn.addEventListener('click', (e) => {
+    tripToRemove = e.target.dataset.item;
+    showConfirmationModal(true);
+  })
+});
+addModalListeners();
+tabs.addEventListener('click', handleTabsClick);
+
+
+function handleTabsClick(e) {
   device = e.target.parentElement.dataset.device;
-
-  console.log( e.target.parentElement);
+  console.log(device)
   for (t of tables){
     if (t.dataset.tab === device)
       t.classList.remove('is-hidden');
@@ -16,8 +31,82 @@ const handleTabsClick = (e) => {
       tab.classList.add('is-active')
     else
       tab.classList.remove('is-active')
-  }
-  
+  }  
 }
 
-tabs.addEventListener('click', handleTabsClick, true);
+
+async function removeTrip() {
+  console.log('Removing '+ tripToRemove);
+  
+  modal.querySelector('button[data-delete]').classList.add('is-loading')
+  bodyRequest = {
+    idTrip: tripToRemove,
+    device: device
+  }
+
+  const response = await fetch('/removeTrip', {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(bodyRequest)
+  });
+  const data = await response.json();
+
+  modal.querySelector('button[data-delete]').classList.remove('is-loading')
+  showConfirmationModal(false);
+  showNotification(data.result);
+  if (data.result == 'success')
+    updateTable()
+}
+
+
+function showConfirmationModal(show){
+  if (show){
+    modal.classList.add('is-active');
+  }
+  else {
+    modal.classList.remove('is-active');
+  }
+}
+
+function showNotification(result) {
+  let notification = document.getElementById('notification');
+  notification.classList.remove('is-hidden');
+
+  if (result === 'success'){
+    notification.classList.add('is-primary');
+    notification.classList.remove('is-danger');
+    notification.firstChild.textContent = 'El trayecto y sus datos han sido borrados correctamente';
+  }
+  else {
+    notification.classList.add('is-danger');
+    notification.classList.remove('is-primary');
+    notification.firstChild.textContent = 'Ocurrió un error al borrar el trayecto';
+  }
+
+  notification.querySelector('.delete').addEventListener('click', (e)=>{
+    notification.classList.add('is-hidden');
+  });
+  setTimeout(() => {
+    notification.classList.add('is-hidden');
+    notification.querySelector('.delete').addEventListener('click', null);
+  }, 10000);
+}
+
+function updateTable() {
+  let row = document.getElementById(tripToRemove);
+  row.parentElement.removeChild(row);
+  tripToRemove = "";
+}
+
+function addModalListeners(){
+  const closeElements = ['.modal-background', 'button[data-cancel]', '.modal-close']
+  for (element of closeElements) {
+    modal.querySelector(element).addEventListener('click', (e) => {
+      e.preventDefault();
+      showConfirmationModal(false);
+    });
+  }
+  modal.querySelector('button[data-delete]').addEventListener('click', removeTrip)
+}
